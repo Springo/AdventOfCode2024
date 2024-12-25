26730f061ff44557a8b40935ac51c15da152f91a
Kevin <kevinmxia@gmail.com>
2024-12-24 20:47:22 -0800

    Day 20.

--- d20.py
+++ d20.py
@@ -12,71 +12,56 @@ def readFile(filename):
     return lines
 
 
-def bfs(grid, start_i, start_j, end_i, end_j, allowed_cuts=0):
+def bfs(grid, start_i, start_j):
     explored = dict()
-    q = [(start_i, start_j, 0, 2, allowed_cuts)]
-    explored[(start_i, start_j)] = True
-    while len(q) > 0:
-        i, j, dist, cut_tiles, cut_times = q.pop(0)
-        if i == end_i and j == end_j:
-            return dist
-        for i2, j2 in gdu.get_neighbors(grid, i, j, orth=True, indices=True):
-            if (i2, j2) not in explored:
-                if grid[i2][j2] == '.' or grid[i2][j2] == 'E':
-                    if cut_tiles != 2:
-                        cut_times = 0
-                    q.append((i2, j2, dist + 1, cut_tiles, cut_times))
-                elif grid[i2][j2] == '#':
-                    if cut_times > 0 and cut_tiles > 0:
-                        q.append((i2, j2, dist + 1, cut_tiles - 1, cut_times))
-                explored[(i2, j2)] = True
-    return -1
-
-
-def bfs_broad(grid, start_i, start_j, end_i, end_j):
-    explored = dict()
-    dists = dict()
+    path = dict()
     q = [(start_i, start_j, 0)]
     explored[(start_i, start_j)] = True
-    dirs = [0, 2, 4, 6]
     while len(q) > 0:
         i, j, dist = q.pop(0)
-
-        for dir in dirs:
-            i2, j2 = gdu.grid_project(grid, i, j, dir, step=1)
-            if i2 is None:
-                continue
-
-            if grid[i2][j2] == '#':
-                i3, j3 = gdu.grid_project(grid, i, j, dir, step=2)
-                if i3 is None:
-                    continue
-
-                if grid[i3][j3] == '.':
-                    new_dist = dist + bfs(grid, i3, j3, end_i, end_j)
-                    if new_dist not in dists:
-                        dists[new_dist] = 0
-                    dists[new_dist] += 1
-                else:
-                    i4, j4 = gdu.grid_project(grid, i, j, dir, step=3)
-                    if i4 is None:
-                        continue
-
-                    if grid[i4][j4] == '.':
-                        new_dist = dist + bfs(grid, i4, j4, end_i, end_j)
-                        if new_dist not in dists:
-                            dists[new_dist] = 0
-                        dists[new_dist] += 1
-
         for i2, j2 in gdu.get_neighbors(grid, i, j, orth=True, indices=True):
             if (i2, j2) not in explored:
-                if grid[i2][j2] == '.':
+                if grid[i2][j2] in {'.', 'E', 'S'}:
                     q.append((i2, j2, dist + 1))
+                    path[(i2, j2)] = ((i, j), dist + 1)
                 explored[(i2, j2)] = True
-    return dists
+    return path
+
+
+def search_shortcut(path, cheat, start_i, start_j):
+    i = start_i
+    j = start_j
+    base = path[(i, j)][1]
+    traveled = 0
+    total = 0
+    saved_dict = dict()
+    seen = set()
+    while (i, j) in path:
+        (ni, nj), dist = path[(i, j)]
+        for (csi, csj) in [(i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1)]:
+            for cei in range(csi - (cheat - 1), csi + (cheat - 1) + 1):
+                rem_cheat = (cheat - 1) - abs(csi - cei)
+                for cej in range(csj - rem_cheat, csj + rem_cheat + 1):
+                    if (i, j, cei, cej) not in seen:
+                        if (cei, cej) in path:
+                            _, cd = path[(cei, cej)]
+                            new_dist = traveled + cd + abs(i - cei) + abs(j - cej)
+                            if new_dist < base:
+                                saved = base - new_dist
+                                if saved > 0:
+                                    saved_dict[saved] = saved_dict.get(saved, 0) + 1
+                                if saved >= 100:
+                                    total += 1
+                        seen.add((i, j, cei, cej))
+        i = ni
+        j = nj
+        traveled += 1
+    return total, saved_dict
+
 
 
 lines = readFile("d20input.txt")
+#lines = readFile("test.txt")
 
 grid = gdu.convert_to_grid(lines)
 si = 0
@@ -92,11 +77,9 @@ for i in range(len(grid)):
             ei = i
             ej = j
 
-base = bfs(grid, si, sj, ei, ej)
-dists = bfs_broad(grid, si, sj, ei, ej)
-
-total = 0
-for dist in dists:
-    if base - dist >= 100:
-        total += dists[dist]
+paths = bfs(grid, ei, ej)
+paths[(ei, ej)] = ((-1, -1), 0)
+total, cuts = search_shortcut(paths, 2, si, sj)
+print(total)
+total, cuts = search_shortcut(paths, 20, si, sj)
 print(total)
